2b237e7b654a2fa0fd58f497f39211aae3be5424
jehling <jehling@users.noreply.github.com>
2021-08-06 16:16:14 -0400

    Vocab table scrape complete. TODO parsing logic & pagination

---
+++ VocabScraper.js
@@ -0,0 +1,40 @@
+// Imports
+var prompt = require('prompt-sync')({sigint: true});
+const {JSDOM} = require('jsdom');
+const jquery = require('jquery');
+const fs = require('fs');
+
+// Constants
+const JLPT_SENSEI_URL_PREFIX = 'https://jlptsensei.com/';
+const JLPT_SENSEI_URL_VOCAB_PREFIX = "jlpt-";
+const JLPT_SENSEI_URL_VOCAB_SUFFIX = "-vocabulary-list/";
+const OUTPUT_DELIMETER = '\t';
+const OUTPUT_DIR = './output_dir/';
+
+// Script
+function getNLevel(){
+    let strN = prompt("Specify JLPT Vocab Level (1-5): ").toLowerCase().trim();
+    let intN = Number(strN);
+    if(!Number.isInteger(intN) || intN < 1 || intN > 5){
+        throw Error(`Invalid input "${strN}".\nPlease enter an integer from 1-5.`);
+    }
+    return intN;
+}
+
+async function getVocabList(nLevel){
+    const JLPT_VOCAB_URL = `jlpt-n${nLevel}-vocabulary-list/`;
+    let dom = await JSDOM.fromURL(JLPT_SENSEI_URL_PREFIX + JLPT_VOCAB_URL);
+    let $ = jquery(dom.window);
+    let vocabTable = $('#jl-vocab:first tbody').children();
+    for (const row of vocabTable){
+        console.log($(row).text());
+    }
+    let vocabList = [];
+}
+
+try{
+    let n = getNLevel();
+    getVocabList(n);
+} catch(err){
+    console.log(err.message);
+}
