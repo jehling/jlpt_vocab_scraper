8cdb2dd105be6bcbbca6bca845fc990b9e56b6c7
jehling <jehling@users.noreply.github.com>
2021-08-06 17:45:59 -0400

    Pagination URL collection implemented.

--- VocabScraper.js
+++ VocabScraper.js
@@ -6,24 +6,21 @@ const fs = require('fs');
 
 // Constants
 const JLPT_SENSEI_URL_PREFIX = 'https://jlptsensei.com/';
-const JLPT_SENSEI_URL_VOCAB_PREFIX = "jlpt-";
-const JLPT_SENSEI_URL_VOCAB_SUFFIX = "-vocabulary-list/";
 const OUTPUT_DELIMETER = '\t';
 const OUTPUT_DIR = './output_dir/';
 
 // Script
-function getNLevel(){
+function getUrl(){
     let strN = prompt("Specify JLPT Vocab Level (1-5): ").toLowerCase().trim();
     let intN = Number(strN);
     if(!Number.isInteger(intN) || intN < 1 || intN > 5){
         throw Error(`Invalid input "${strN}".\nPlease enter an integer from 1-5.`);
     }
-    return intN;
+    return `${JLPT_SENSEI_URL_PREFIX}jlpt-n${intN}-vocabulary-list/`;
 }
 
-async function getVocabList(nLevel){
-    const JLPT_VOCAB_URL = `jlpt-n${nLevel}-vocabulary-list/`;
-    let dom = await JSDOM.fromURL(JLPT_SENSEI_URL_PREFIX + JLPT_VOCAB_URL);
+async function getVocabList(url){
+    let dom = await JSDOM.fromURL(url);
     let $ = jquery(dom.window);
     let vocabTable = $('#jl-vocab:first tbody').children();
     let vocabList = [];
@@ -37,9 +34,25 @@ async function getVocabList(nLevel){
     return vocabList;
 }
 
+async function paginationUrlList(rootUrl){
+    let dom = await JSDOM.fromURL(rootUrl);
+    let $ = jquery(dom.window);
+    let pageUrlList = [rootUrl];
+    let paginationBar = $('a.page-numbers');
+    for (const pageLink of paginationBar){
+        if($(pageLink).attr('class') != "page-numbers") continue;
+        let url = $(pageLink).attr('href');
+        pageUrlList.push(url);
+    }
+    console.log(`${pageUrlList.length} PAGES FOUND`);
+    return pageUrlList;
+}
+
 try{
-    let n = getNLevel();
-    getVocabList(n);
+    let url = getUrl();
+    getVocabList(url);
+    paginationUrlList(url).then(console.log);
+    
 } catch(err){
     console.log(err.message);
 }
