ea212ab80c38133aa5d280634f7b2307fb327daf
jehling <jehling@users.noreply.github.com>
2021-08-06 23:57:01 -0400

    Reworked file printouts to be more descriptive

--- VocabScraper.js
+++ VocabScraper.js
@@ -8,16 +8,20 @@ const fs = require('fs');
 const JLPT_SENSEI_URL_PREFIX = 'https://jlptsensei.com/';
 const OUTPUT_DIR = './output_dir/';
 const OUTPUT_DELIMETER = "\n";
-const OUTPUT_FILE = "vocabList.txt";
+const OUTPUT_FILE_SUFFIX = "vocab_list.txt";
+let JLPT_LEVEL = "";
+let TOTAL_TERM_COUNT = "";
+let CURR_TERM_COUNT = 0;
 
 // Script
 function getUrl(){
     let strN = prompt("Specify JLPT Vocab Level (1-5): ").toLowerCase().trim();
     let intN = Number(strN);
     if(!Number.isInteger(intN) || intN < 1 || intN > 5){
-        throw Error(`Invalid input "${strN}".\nPlease enter an integer from 1-5.`);
+        throw Error(`INVALID INPUT "${strN}".\nPlease enter an integer from 1-5.`);
     }
-    return `${JLPT_SENSEI_URL_PREFIX}jlpt-n${intN}-vocabulary-list/`;
+    JLPT_LEVEL = `n${strN}`;
+    return `${JLPT_SENSEI_URL_PREFIX}jlpt-${JLPT_LEVEL}-vocabulary-list/`;
 }
 
 async function getVocabList(url){
@@ -30,13 +34,15 @@ async function getVocabList(url){
         if(term.length == 0) continue;
         vocabList.push(term);
     }
-    console.log(`${vocabList.length} TERMS GENERATED`);
+    CURR_TERM_COUNT += vocabList.length;
+    console.log(`${CURR_TERM_COUNT} / ${TOTAL_TERM_COUNT} Terms Generated`);
     return vocabList;
 }
 
 async function getPaginationURLs(rootUrl){
     let dom = await JSDOM.fromURL(rootUrl);
     let $ = jquery(dom.window);
+    TOTAL_TERM_COUNT = $('h3.lessons-available').text().trim().match(/\d\d+/)[0];
     let pageUrlList = [rootUrl];
     let paginationBar = $('a.page-numbers');
     for (const pageLink of paginationBar){
@@ -44,7 +50,8 @@ async function getPaginationURLs(rootUrl){
         let url = $(pageLink).attr('href');
         pageUrlList.push(url);
     }
-    console.log(`${pageUrlList.length} PAGES FOUND`);
+    let printout = `${pageUrlList.length} ${(pageUrlList.length > 1? `PAGES` : `PAGE`)} FOUND`;
+    console.log(printout);
     return pageUrlList;
 }
 
@@ -56,22 +63,23 @@ async function getMasterVocabList(){
         let vocabList = await getVocabList(url);
         masterVocabList = masterVocabList.concat(vocabList);   
     }
-    console.log(`${masterVocabList.length} TERM MASTER VOCAB LIST COMPLETED`);
+    console.log(`${masterVocabList.length} Term Vocab List Competed`);
     return masterVocabList;
 }
 
 function genFile(vocabList){
     let fileData = "";
+    let outputFile = `${JLPT_LEVEL}_${OUTPUT_FILE_SUFFIX}`;
     for(const term of vocabList){
         fileData += `${term}${OUTPUT_DELIMETER}`;
     }
-    fs.writeFile(path=OUTPUT_DIR + OUTPUT_FILE, data=fileData, callback= () => console.log(`Writing Complete - File: ${OUTPUT_DIR + OUTPUT_FILE} - Terms Generated: ${vocabList.length}.`));
+    fs.writeFile(path=OUTPUT_DIR + outputFile, data=fileData, callback= () => console.log(`Writing Complete - File: ${OUTPUT_DIR + outputFile} - Terms Generated: ${vocabList.length}.`));
 }
 
 function main(){
     fs.rmSync(OUTPUT_DIR, { force: true, recursive: true});
     fs.mkdir(OUTPUT_DIR, dir_err => {
-        if(dir_err) return console.error(err);
+        if(dir_err) return console.error(dir_err);
         getMasterVocabList().then(genFile).catch(error => console.log(error.message));
     });
 }
